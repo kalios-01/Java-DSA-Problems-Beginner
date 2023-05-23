71bd9f65d2065dd979eacf3767de1c7291e1bd64
kalios-01 <priyanshu07893@gmail.com>
2023-05-24 03:15:15 +0530

    Bitmanupulation is add

---
+++ Bit-Manupulation/PowerofTwoChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * PowerofTwoChecker
+ */
+public class PowerofTwoChecker {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        int number = sn.nextInt();
+        if(isPowerTwo(number)){
+            System.out.println(number+"is power of 2");
+        }
+        else{
+            System.out.println(number+"is Not power of 2");
+        }
+        
+        
+    }
+    public static boolean isPowerTwo(int number){
+        if(number <= 0){
+            return false;
+        }
+        else{
+
+            return (number &(number-1))==0;
+        }
+
+    }    
+}
